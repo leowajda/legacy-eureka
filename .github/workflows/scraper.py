1f079fe8a23a3e6d1bb7e22bab133af3018fcd36
Leonardo Wajda <dev.leowajda@tuta.io>
2022-11-10 20:06:18 +0100

    :construction_worker: Update submodule

--- .github/workflows/scraper.py
+++ .github/workflows/scraper.py
@@ -4,12 +4,7 @@ from functools import reduce
 
 p = Path('.')
 subdirs = [x for x in p.iterdir() if x.is_dir() and 'eureka-' in x.name]
-
-for dir in subdirs:
-    for file in dir.iterdir():
-        print(file)
-
-frames = [pd.read_csv(f'/home/runner/work/eureka-scraper/eureka-scraper/{dir}/docs/data.csv') for dir in subdirs]
+frames = [pd.read_csv(f'./{dir}/docs/data.csv') for dir in subdirs]
 
 join = reduce(lambda left, right: pd.merge(left, right, how='outer', on=['ID', 'Name']), frames)
 join = join.set_index('ID').fillna('').sort_index()
