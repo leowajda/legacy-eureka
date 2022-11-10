5bb9943e7dab470f68d1e87bc30ceac11ce02106
Leonardo Wajda <dev.leowajda@tuta.io>
2022-11-10 20:04:11 +0100

    :alembic: Check submodule files

--- .github/workflows/scraper.py
+++ .github/workflows/scraper.py
@@ -3,8 +3,12 @@ from pathlib import Path
 from functools import reduce
 
 p = Path('.')
-subdirs = [(x / 'docs' / 'data.csv').exists() for x in p.iterdir() if x.is_dir() and 'eureka-' in x.name]
-print(subdirs)
+subdirs = [x for x in p.iterdir() if x.is_dir() and 'eureka-' in x.name]
+
+for dir in subdirs:
+    for file in dir.iterdir():
+        print(file)
+
 frames = [pd.read_csv(f'/home/runner/work/eureka-scraper/eureka-scraper/{dir}/docs/data.csv') for dir in subdirs]
 
 join = reduce(lambda left, right: pd.merge(left, right, how='outer', on=['ID', 'Name']), frames)
